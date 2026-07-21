2c65c8ed72a2f69d5bf2648a3261445be53c9dee
black-tea-2025 <solvene-lj@infoseek.jp>
2026-07-21 12:57:06 +0900

    Fix iPhone home-screen icon

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'swallow-recipe-book-v3';
-const ASSETS = ['./', './index.html', './styles.css', './app.js', './manifest.webmanifest', './icons/icon.svg', './icons/icon-192.png', './icons/icon-512.png', './icons/apple-touch-icon.png'];
+const CACHE = 'swallow-recipe-book-v4';
+const ASSETS = ['./', './index.html', './styles.css', './app.js', './manifest.webmanifest', './apple-touch-icon.png', './apple-touch-icon-180x180.png', './icons/icon.svg', './icons/icon-192.png', './icons/icon-512.png', './icons/apple-touch-icon.png'];
 self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting())));
 self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim())));
 self.addEventListener('fetch', event => {
