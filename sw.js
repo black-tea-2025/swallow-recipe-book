a5584e6ab94def8b3364a9587daafda7f8b88f47
black-tea-2025 <solvene-lj@infoseek.jp>
2026-07-21 13:37:49 +0900

    Restore light swallow icon

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'swallow-recipe-book-v5';
+const CACHE = 'swallow-recipe-book-v7';
 const ASSETS = ['./', './index.html', './styles.css', './app.js', './manifest.webmanifest', './apple-touch-icon.png', './apple-touch-icon-180x180.png', './icons/icon.svg', './icons/icon-192.png', './icons/icon-512.png', './icons/apple-touch-icon.png'];
 self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting())));
 self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim())));
